71ea59a0706e436b5abf465dc4c81802406e7da0
kong91186 <kong91186@gmail.com>
2026-01-09 17:34:13 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Co2Controller } from './co2/co2.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, Co2Controller], // ✅ เพิ่มตรงนี้
   providers: [AppService],
 })
 export class AppModule {}
