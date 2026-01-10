e1c8d2aa83e690f63e8c1d411fc90118c9d2ccc8
kong91186 <kong91186@gmail.com>
2026-01-10 12:13:03 +0700

    Update co2.controller.ts

--- src/co2/co2.controller.ts
+++ src/co2/co2.controller.ts
@@ -1,28 +1,17 @@
-<<<<<<< HEAD
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('co2')
-export class Co2Controller {
-  @Get()
-  getAll() {
-    return { message: 'co2 api works' };
-  }
-}
-=======
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { firestore } from '../firebase/firebase-admin';
 
 @Controller('co2')
 export class Co2Controller {
 
-  // ✅ รับข้อมูลจากเว็บคุณ
+  // รับข้อมูลจากเว็บ (POST)
   @Post()
   async create(@Body() data: any) {
     await firestore.collection('travel_logs').add(data);
     return { message: 'saved' };
   }
 
-  // ✅ ให้เพื่อนดึงข้อมูลไปใช้
+  // ให้เพื่อนดึงข้อมูลไปใช้ (GET)
   @Get()
   async getAll() {
     const snapshot = await firestore
@@ -37,4 +26,3 @@ export class Co2Controller {
   }
 }
 
->>>>>>> 6edf338e (fix: GET /co2 return firestore data)
