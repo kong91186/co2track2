c927e4b48ea270f9c45f2b055e42a288b30e00dd
kong91186 <kong91186@gmail.com>
2026-01-10 12:34:45 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Co2Controller } from './co2/co2.controller';
- HEAD
-
-@Module({
-  imports: [],
-  controllers: [AppController, Co2Controller], // ✅ เพิ่มตรงนี้
-  providers: [AppService],
-
 import { Co2Service } from './co2/co2.service';
 
 @Module({
@@ -21,6 +14,5 @@ import { Co2Service } from './co2/co2.service';
     AppService,
     Co2Service,
   ],
-  6edf338e (fix: GET /co2 return firestore data)
 })
 export class AppModule {}
